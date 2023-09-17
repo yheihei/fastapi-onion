ba6af64bfc5d44c66aa63212eadc4c581b01230b
yheihei <yhei.register@gmail.com>
2023-09-17 17:01:12 +0900

    タスク作成

---
+++ api/models/task.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+from api.db import Base
+
+
+class Task(Base):
+    __tablename__ = "task"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(1024))
+
+    done = relationship("Done", back_populates="task", cascade="delete")
+
+
+class Done(Base):
+    __tablename__ = "done"
+
+    id = Column(Integer, ForeignKey("task.id"), primary_key=True)
+
+    task = relationship("Task", back_populates="done")
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,6 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import api.cruds.task as task_crud
+from api.db import get_db
 
 import api.schemas.task as task_schema
 
@@ -14,8 +18,8 @@ async def list_tasks():
 
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.model_dump())
+async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+    return await task_crud.create_task(db, task_body)
 
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
