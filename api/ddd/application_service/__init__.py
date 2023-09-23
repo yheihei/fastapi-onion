4e19c4c6cc7ec487472f4205eab33896fabf5afa
yheihei <yhei.register@gmail.com>
2023-09-23 12:44:54 +0900

    updateのAppService

--- api/ddd/application_service/__init__.py
+++ api/ddd/application_service/__init__.py
@@ -1,7 +1,9 @@
 from .task_create_application_service import TaskCreateApplicationService
 from .task_list_get_application_service import TaskListGetApplicationService
+from .task_update_application_service import TaskUpdateApplicationService
 
 __all__ = [
     "TaskCreateApplicationService",
     "TaskListGetApplicationService",
+    "TaskUpdateApplicationService",
 ]
--- api/routers/task.py
+++ api/routers/task.py
@@ -7,7 +7,8 @@ import api.cruds.task as task_crud
 import api.schemas.task as task_schema
 from api.db import get_db
 from api.ddd.application_service import (TaskCreateApplicationService,
-                                         TaskListGetApplicationService)
+                                         TaskListGetApplicationService,
+                                         TaskUpdateApplicationService)
 from api.ddd.repository.task_repository import TaskRepository
 
 router = APIRouter()
@@ -33,11 +34,9 @@ async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depe
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_id: int, task_body: task_schema.TaskCreate,  db: AsyncSession = Depends(get_db)):
-    task = await task_crud.get_task(db, task_id)
-    if task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
-    hoge = await task_crud.update_task(db, task_body, task)
-    return hoge
+    repository = TaskRepository(db)
+    task = await TaskUpdateApplicationService(repository).update(task_id, task_body.title)
+    return task_schema.TaskCreateResponse(id=task.id.value(), title=task.title.value())
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
